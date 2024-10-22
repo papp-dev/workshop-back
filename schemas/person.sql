82b4bc7bb9c45ab8f4b4e1faf301f1d2d28eb627
pauloapanucci <pauloapanucci@gmail.com>
2024-10-22 10:50:48 -0300

    increase weight and height mysql column

--- schemas/person.sql
+++ schemas/person.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS
         `id` INT NOT NULL AUTO_INCREMENT,
         `resource_id` VARCHAR(36) NOT NULL,
         `name` VARCHAR(512),
-        `weight` DECIMAL (10,8),
-        `height` DECIMAL (10,8),
+        `weight` DECIMAL (15,8),
+        `height` DECIMAL (15,8),
         PRIMARY KEY (`id`),
         CONSTRAINT uc_person_resource_id UNIQUE(`resource_id`)
     );
